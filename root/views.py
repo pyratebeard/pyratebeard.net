871eab823d5965e14f306712742fa32ab444d451
pyratebeard <root@pyratebeard.net>
2016-08-23 09:17:00 +0100

    root app views and urls

--- root/views.py
+++ root/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
-# Create your views here.
+def root(request):
+  # request the context of the request.
+  # the context contains information such as the client's machine details, for example
+  context = RequestContext(request)
+
+  # construct a dictionary to pass to the template engine as its context.
+  content = {}
+
+  return render_to_response('root/root.html', content, context)
+
