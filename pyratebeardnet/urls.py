9cb4616a0fec02e89210c37f78f4edfec3899bfa
pyratebeard <root@pyratebeard.net>
2016-09-04 18:34:24 +0100

    removed empty string from list

--- pyratebeardnet/urls.py
+++ pyratebeardnet/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = ('',
+urlpatterns = (
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('root.urls')),
 )
--- root/urls.py
+++ root/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     url(r'^$', 'root.views.root'),
 )
