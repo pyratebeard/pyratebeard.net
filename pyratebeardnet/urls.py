b64a783a2ed0097cd55ec406c5ae8b0c11ecc0c3
pyratebeard <root@pyratebeard.net>
2016-09-08 14:51:00 +0100

    fixed url files, needed 'patterns' declared

--- pyratebeardnet/urls.py
+++ pyratebeardnet/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = (
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('root.urls')),
 )
