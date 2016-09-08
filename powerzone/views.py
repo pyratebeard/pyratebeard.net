c2414e9f3034afbc74dd6348061ba7b02683b53b
pyratebeard <root@pyratebeard.net>
2016-09-08 19:18:18 +0100

    powerzone app started. template added

---
+++ powerzone/views.py
@@ -0,0 +1,14 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+def powerzone(request):
+  # request the context of the request.
+  # the context contains information such as the client's machine details, for example
+  context = RequestContext(request)
+
+  # construct a dictionary to pass to the template engine as its context.
+  content = {}
+
+  return render_to_response('powerzone/powerzone.html', content, context)
+
