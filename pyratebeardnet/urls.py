063ae5c61997925ecdb21e0534793a1ee2b4f804
pyratebeard <root@pyratebeard.net>
2016-10-02 18:52:16 +0100

    log url and link added

--- pyratebeardnet/urls.py
+++ pyratebeardnet/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('root.urls')),
-    url(r'^blog', include('blog.urls')),
+    url(r'^log', include('log.urls')),
     url(r'^mcc', include('mcc.urls')),
     url(r'^powerzone', include('powerzone.urls')),
 )
