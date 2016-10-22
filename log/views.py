8bd30b2cae333fabaa7d99c063195247f95120de
dudley <pyratebeard@gmail.com>
2016-10-22 19:38:06 +0100

    tidy up - remove comments

--- log/views.py
+++ log/views.py
@@ -24,14 +24,3 @@ def view_category(request, slug):
         'category': category,
         'posts': Blog.objects.filter(category=category)[:5]
     })
-
-# def blog(request):
-#   # request the context of the request.
-#   # the context contains information such as the client's machine details, for example
-#   context = RequestContext(request)
-
-#   # construct a dictionary to pass to the template engine as its context.
-#   content = {}
-
-#   return render_to_response('blog/blog.html', content, context)
-
